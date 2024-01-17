ed22565d18138f9b4d9433002789218d1a213f8a
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-17 13:53:03 +0200

    Add files via upload

---
+++ Set2 ex06/Set2 ex6/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Set2_ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+
+            bool esteCrescatoare = true;
+
+            Console.Write($"Introduceti numarul de pe pozitia {0}: ");
+            int x = int.Parse(Console.ReadLine());
+            
+            for (int i = 1; i < n; i++) 
+            {
+                Console.Write($"Introduceti numarul de pe pozitia {i}: ");
+                int y = int.Parse(Console.ReadLine());
+
+                if (x > y)
+                    esteCrescatoare = false;                 
+                x = y;
+            }
+            if (esteCrescatoare)
+                Console.WriteLine("Secventa este crescatoare.");
+            else
+                Console.WriteLine("Secventa NU este crescatoare.");
+
+        }
+    }
+}
