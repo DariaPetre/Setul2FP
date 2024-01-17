8387c4d0d9352e3850ef972259360d2f0629ae8a
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-17 13:50:01 +0200

    Add files via upload

---
+++ Set2 ex02/Set2 ex 2/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Set2_ex_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+
+            int cateNegative = 0;
+            int catePozitive = 0;
+            int cateSuntZero = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.Write($"Introduceti numarul de pe pozitia {i}: ");
+                int x = int.Parse(Console.ReadLine());
+
+                if (x < 0)
+                    cateNegative++;
+
+                if (x == 0)
+                    cateSuntZero++;
+
+                if (x > 0)
+                    catePozitive++;
+            }
+
+            Console.WriteLine($"In secventa sunt {cateNegative} numere negative.");
+            Console.WriteLine($"In secventa sunt {cateSuntZero} numere egale cu 0.");
+            Console.WriteLine($"In secventa sunt {catePozitive} numere pozitive.");
+        }
+    }
+}
