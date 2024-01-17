f9167fa09e7c847527c135168644fe3af4c3355b
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-17 13:51:37 +0200

    Add files via upload

---
+++ Set2 ex04/Set2 ex4/Program.cs
@@ -0,0 +1,32 @@
+﻿
+namespace Set2_ex4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("a=");
+            int a = int.Parse(Console.ReadLine());
+
+
+            int poz = -1;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"Introduceti numarul de pe pozitia {i}: ");
+                int x = int.Parse(Console.ReadLine());
+
+                if (x == a)
+                {                   
+                    poz = i;
+                }
+                    
+            }
+            Console.WriteLine(poz);
+            
+        }
+    }
+}
