ef766c87b1c3d257c608861b1612371b18a457f6
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-17 13:57:20 +0200

    Add files via upload

---
+++ Set2 ex11/Set2 ex11/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Set2_ex11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+
+            double suma = 0;
+
+            for(int i = 0; i < n; i++) 
+            {
+                Console.Write($"Introduceti numarul de pe pozitia {i}: ");
+                double x = double.Parse(Console.ReadLine());
+                suma = suma + (1 / x);
+
+            }
+            Console.WriteLine(suma);
+        }
+    }
+}
