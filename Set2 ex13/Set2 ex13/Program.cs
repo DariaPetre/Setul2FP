f3f852809f71d0e8f1d8da7c43121ec4c4b4025e
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-17 13:58:40 +0200

    Add files via upload

---
+++ Set2 ex13/Set2 ex13/Program.cs
@@ -0,0 +1,37 @@
+﻿
+namespace Set2_ex13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write($"Introduceti numarul de pe pozitia {0}: ");
+            int x = int.Parse(Console.ReadLine());
+
+            int primulNumar = x;
+            int contor = 0;
+            /*
+              Conditii:
+              -sa existe maxim doua subsecvente ordonate crescator
+              -(primul numar) > (ultimul numar)
+            */
+            for (int i = 1; i < n; i++) 
+            {
+                Console.Write($"Introduceti numarul de pe pozitia {i}: ");
+                int y = int.Parse(Console.ReadLine());
+                if (x > y)
+                    contor++;
+                    
+                x = y;
+            }
+           if( contor <= 1 && primulNumar > x) 
+                Console.WriteLine("Secventa este crescatoare rotita.");
+            else
+                Console.WriteLine("Secventa NU este crescatoare rotita.");
+
+        }
+    }
+}
