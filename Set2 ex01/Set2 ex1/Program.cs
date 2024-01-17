8d518341b476731c7bdd693924a99e74071647ef
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-17 13:49:23 +0200

    Add files via upload

---
+++ Set2 ex01/Set2 ex1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace Set2_ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+
+            int catePare = 0;
+
+            for (int i = 1; i <= n; i++) 
+            {
+                Console.Write($"Introduceti numarul de pe pozitia {i}: ");
+                int x = int.Parse(Console.ReadLine());
+                if( x % 2 == 0 )
+                {
+                    catePare++;
+                }
+            }
+
+            Console.WriteLine($"In secventa sunt {catePare} numere pare.");
+            
+        }
+    }
+}
