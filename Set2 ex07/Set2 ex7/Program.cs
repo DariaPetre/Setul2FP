9ef0fd216163a83f8ae8d5f077a6b71da34f3214
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-17 13:53:50 +0200

    Add files via upload

---
+++ Set2 ex07/Set2 ex7/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Set2_ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+
+            int max = -2147483647 - 1;
+            int min = 2147483647;
+           
+
+
+            for (int i = 0; i < n; i++) 
+            {
+                Console.Write($"Introduceti numarul de pe pozitia {i}: ");
+
+                int x = int.Parse(Console.ReadLine());
+
+                if (x > max)
+                    max = x;
+                if (x < min)
+                    min = x;
+            }
+            Console.WriteLine($"Minimul secventei este {min}.");
+            Console.WriteLine($"Maximul secventei este {max}.");
+
+        }
+    }
+}
