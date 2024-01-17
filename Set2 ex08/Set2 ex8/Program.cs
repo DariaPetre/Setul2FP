56984b6cb1c6977d608c8870c56b6e220bbaaafb
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-17 13:54:30 +0200

    Add files via upload

---
+++ Set2 ex08/Set2 ex8/Program.cs
@@ -0,0 +1,33 @@
+﻿
+namespace Set2_ex8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(Fibo(n));
+        }
+        static int Fibo(int n)
+        {
+            int k = 2, a, b, c;
+            a = 0;
+            b = 1;
+            if (n == 1) return 0;
+             else if (n == 2) return 1;
+                else
+                {
+                    while (k < n)
+                    {
+                        c = a + b;
+                        a = b;
+                        b = c;
+                        k++;
+                    }
+                 
+                }
+            return b;
+        }
+    }
+}
