c3c9db2a761bb35131856a9a6724dbaa58546558
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-17 13:56:21 +0200

    Add files via upload

---
+++ Set2 ex10/Set2 ex10/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Set2_ex10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write($"Introduceti numarul de pe pozitia {0}: ");
+            int x = int.Parse(Console.ReadLine());
+
+            int lungime = 1;
+            int lmax = 1;
+
+            for (int i = 1; i < n; i++) 
+            {
+                Console.Write($"Introduceti numarul de pe pozitia {i}: ");
+                int y = int.Parse(Console.ReadLine());
+
+                if (x == y)
+                {
+                    lungime++;
+                    if (lungime > lmax)
+                        lmax = lungime;
+                }
+                else lungime = 1;
+
+                x = y;
+            }
+            Console.WriteLine(lmax);
+        }
+    }
+}
