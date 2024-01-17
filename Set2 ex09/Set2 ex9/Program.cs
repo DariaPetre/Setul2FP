24e4c8ff1243106bbe171e7e83848bcdf7f04a5a
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-17 13:55:25 +0200

    Add files via upload

---
+++ Set2 ex09/Set2 ex9/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Set2_ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+
+            bool esteMonotonCrescatoare = true;
+            bool esteMonotonDescrescatoare = true;
+
+            Console.Write($"Introduceti numarul de pe pozitia {0}: ");
+
+            int x = int.Parse(Console.ReadLine());
+            for (int i = 1; i < n; i++)
+            {
+                Console.Write($"Introduceti numarul de pe pozitia {i}: ");
+                int y = int.Parse(Console.ReadLine());
+
+                if (x > y)
+                    esteMonotonCrescatoare = false;
+                if (x < y)
+                    esteMonotonDescrescatoare = false;
+                x = y;
+            }
+            if ( esteMonotonCrescatoare || esteMonotonDescrescatoare )
+                Console.WriteLine("Secventa este monotona.");
+            else
+                Console.WriteLine("Secventa NU este monotona.");
+
+        }
+    }
+}
