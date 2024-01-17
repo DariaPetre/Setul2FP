47a2bc1a70b8034abf64334fc52dbf51d5363112
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-17 13:59:59 +0200

    Add files via upload

---
+++ Set2 ex15/Set2 ex15/Program.cs
@@ -0,0 +1,51 @@
+﻿
+namespace Set2_ex15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write($"Introduceti numarul de pe pozitia {0}: ");
+            int x = int.Parse(Console.ReadLine());
+
+            bool bitonica = true;
+            bool crescatoare = false;
+            bool descrescatoare = false;
+
+            
+
+            for (int i = 1; i < n; i++) 
+            {
+                Console.Write($"Introduceti numarul de pe pozitia {i}: ");
+                int y = int.Parse(Console.ReadLine());
+
+                if (!crescatoare && descrescatoare)
+                    bitonica = false;
+                
+                if(x < y)
+                    crescatoare = true;
+                if (x > y)
+                    descrescatoare = true;
+               
+                if(crescatoare && descrescatoare && x<y)
+                    bitonica = false;
+                
+
+                x = y;
+            }
+
+            if((crescatoare && !descrescatoare)|| (!crescatoare && descrescatoare))
+                bitonica = false;
+
+            if (bitonica)
+                Console.WriteLine("Secventa este bitonica.");
+
+            else
+                Console.WriteLine("Secventa NU este bitonica.");
+                
+        }
+    }
+}
