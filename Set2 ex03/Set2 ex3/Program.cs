d268d24dad5963be812637c3f3e368d0e8abb1e1
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-17 13:50:58 +0200

    Add files via upload

---
+++ Set2 ex03/Set2 ex3/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Set2_ex3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+
+            int suma = 0;
+            decimal produs = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                suma = suma + i;
+                produs = produs * i;
+            }
+            Console.WriteLine($"Suma numerelor de la 1 la n este {suma}");
+            Console.WriteLine($"Produsul numerelor de la 1 la n este {produs}");
+
+        }
+    }
+}
