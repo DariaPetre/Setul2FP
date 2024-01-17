fc6c3a590dbaa50df673a36cb1f5bf6abd2d6b45
DariaPetre <96527117+DariaPetre@users.noreply.github.com>
2024-01-17 13:52:23 +0200

    Add files via upload

---
+++ Set2 ex05/Set2 ex5/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+namespace Set2_ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n=");
+            int n = int.Parse(Console.ReadLine());
+
+            int cateEgaleCuPoz = 0;
+
+            for(int i = 0; i < n; i++) 
+            {
+                Console.Write($"Introduceti numarul de pe pozitia {i}: ");
+                int x = int.Parse(Console.ReadLine());
+
+                if (x == i)
+                {
+                    cateEgaleCuPoz++;
+                }
+            }
+            Console.WriteLine(cateEgaleCuPoz);
+        }
+    }
+}
